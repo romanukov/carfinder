e311a6e769c1fd1ef677382a10d8191e8c30050c
Eryn Drem <cats.7777@mail.ru>
2021-03-06 02:17:35 +0300

    Add account db repo methods

--- sources/adapters/psql_database/accounts/repos.py
+++ sources/adapters/psql_database/accounts/repos.py
@@ -8,7 +8,7 @@ from core.accounts.repos import IAccountsRepo
 
 
 class PsqlAccountsRepo(BasePsqlRepo, IAccountsRepo):
-    
+
     def create(self, email: str, password_hash: str, first_name: str, last_name: str) -> Account:
         account = AccountsTable(
             email=email,
@@ -19,11 +19,11 @@ class PsqlAccountsRepo(BasePsqlRepo, IAccountsRepo):
                 last_name=last_name,
             ),
         )
-        
+
         self.session.begin()
         self.session.add(account)
         self.session.commit()
-        
+
         return present_account(account)
 
     def get_by_email(self, email: str) -> Optional[Account]:
@@ -34,20 +34,53 @@ class PsqlAccountsRepo(BasePsqlRepo, IAccountsRepo):
         account = self.session.query(AccountsTable).get(email)
         if not account:
             return None
-        
+
         if account.password_hash != password_hash:
             return None
-        
+
         return present_account(account)
 
     def update_email(self, old_email: str, new_email: str) -> None:
-        pass
+        account = self.session.query(AccountsTable).get(old_email)
+        if not account:
+            return None
 
-    def update_personal_data(self, first_name: str, last_name: str) -> None:
-        pass
+        account.email = new_email
+
+        self.session.begin()
+        self.session.add(account)
+        self.session.commit()
+
+    def update_personal_data(self, email: str, first_name: str, last_name: str) -> None:
+        account = self.session.query(AccountsTable).get(email)
+        if not account:
+            return None
+
+        account.personal_data = PersonalDataTable(
+            first_name=first_name,
+            last_name=last_name,
+        ),
+
+        self.session.begin()
+        self.session.add(account)
+        self.session.commit()
 
     def update_password(self, email: str, new_password_hash: str) -> None:
-        pass
+        account = self.session.query(AccountsTable).get(email)
+        if not account:
+            return None
+
+        account.password_hash = new_password_hash
+
+        self.session.begin()
+        self.session.add(account)
+        self.session.commit()
 
     def delete(self, email: str) -> None:
-        pass
+        account = self.session.query(AccountsTable).get(email)
+        if not account:
+            return None
+
+        self.session.begin()
+        self.session.delete(account)
+        self.session.commit()
